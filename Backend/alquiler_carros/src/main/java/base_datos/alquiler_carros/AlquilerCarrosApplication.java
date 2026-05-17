2556dc0db1151e1b38e6038fc6ffd7d54ad16f5a
Esteban Alexander Bautista Solano <72938908+EstebanBautista24@users.noreply.github.com>
2026-05-17 13:12:19 -0500

    Creacion inicial del proyecto en spring boot para el backend

---
+++ Backend/alquiler_carros/src/main/java/base_datos/alquiler_carros/AlquilerCarrosApplication.java
@@ -0,0 +1,13 @@
+package base_datos.alquiler_carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilerCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilerCarrosApplication.class, args);
+	}
+
+}
---
+++ Backend/alquiler_carros/src/test/java/base_datos/alquiler_carros/AlquilerCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package base_datos.alquiler_carros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
